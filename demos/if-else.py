b3aaf99f975d9a9858db3b243aa0e4ae22d428c1
Mohammed Aeraf <aeraf@outlook.com>
2026-07-29 17:48:27 +0530

    Conditional Statements (If Else)

---
+++ demos/if-else.py
@@ -0,0 +1,7 @@
+age = 15
+
+if age >= 18:
+    print("You are eligible to vote!")
+else:
+    print("You are not eligible to vote!")
+    
---
+++ demos/odd-even.py
@@ -0,0 +1,15 @@
+# Program to read a number and output if it's odd or even
+# Odd Nos - 1,3,5,7...
+# Even Nos - 2,4,6,8... (Numbers divisible by 2)
+
+n = int(input("Enter a number: "))
+rem = n % 2
+# print("Remainder =", rem)
+
+if rem == 0:
+    print(n, "is even")
+else:
+    print(n, "is odd")
+
+
+
