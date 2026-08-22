61c0247863e27885666d36c596935fe2d98aafb4
mohammedaeraf <aeraf@outlook.com>
2026-08-22 17:52:15 +0530

    Strings Demo

---
+++ demos/strings.py
@@ -0,0 +1,31 @@
+course = "python"
+n = 10
+print(course[0])
+print(len(course))
+print(course[0:5])
+print(course[-1])
+
+print(course[0:])
+print(course[:3])
+
+print(course * 3)
+
+name = "abbas attar"
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+
+if "arif" in name:
+    print("Arif exists in the name")
+else:
+    print("Arif doesn't exist in the name")
+text = input("Enter a word:")
+reverse = text[::-1]
+
+if text == reverse:
+    print("The text you entered",text,"is palindrome")
+else:
+    print("The text you entered",text,"is not a palindrome")
+    
+
